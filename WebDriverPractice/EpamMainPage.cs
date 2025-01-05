6a76c4614206ea1be1d1c2894daec8138680a3ca
Slawomir Was <was.slawek@gmail.com>
2025-01-05 15:25:42 +0100

    start implementation of Page Object for Epam Main Page

---
+++ WebDriverPractice/EpamMainPage.cs
@@ -0,0 +1,20 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebDriverPractice
+{
+	public class EpamMainPage
+	{
+		private readonly IWebDriver driver;
+
+
+		public EpamMainPage(IWebDriver driver)
+        {
+			this.driver = driver;
+		}
+    }
+}
