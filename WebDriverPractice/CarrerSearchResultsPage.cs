6215c40811740912ed888850cb301a91fc2c815e
Slawomir Was <was.slawek@gmail.com>
2025-01-07 20:59:34 +0100

    create CarrerSearchResultPage

---
+++ WebDriverPractice/CarrerSearchResultsPage.cs
@@ -0,0 +1,19 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebDriverPractice
+{
+	public class CarrerSearchResultsPage
+	{
+		private readonly IWebDriver _driver;
+
+		public CarrerSearchResultsPage(IWebDriver driver)
+        {
+			_driver = driver;
+		}
+    }
+}
