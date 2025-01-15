b815bb4eac19dce9e451b5b6ab46f3a5ace949b4
Slawomir Was <was.slawek@gmail.com>
2025-01-15 23:16:35 +0100

    update DataConstants

---
+++ WebDriverPractice/Data/DataConstants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebDriverPractice.Data
+{
+	public static class DataConstants
+	{
+		public const string DownloadFilePath = "C:\\Users\\wassl\\Downloads\\EPAM_Corporate_Overview_Q4_EOY.pdf";
+	}
+}
