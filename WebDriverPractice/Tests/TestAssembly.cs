29c46de47a558b51e3038033d2eea4cc9b8c2372
Slawomir Was <was.slawek@gmail.com>
2025-02-05 20:37:56 +0100

    move assembly setup to a new class

---
+++ WebDriverPractice/Tests/TestAssembly.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Serilog;
+using WebDriverPractice.Core.Logging;
+
+namespace WebDriverPractice.Tests
+{
+	[TestClass]
+	public static class TestAssembly
+	{
+
+		[AssemblyInitialize]
+		public static void AssemblyInitialize(TestContext context)
+		{
+			LogManager.Initialize();
+			Log.Information("Global test setup started.");
+		}
+
+		[AssemblyCleanup]
+		public static void AssemblyCleanup()
+		{
+			Log.Information("Assembly cleanup.");
+			LogManager.Close();
+		}
+	}
+}
