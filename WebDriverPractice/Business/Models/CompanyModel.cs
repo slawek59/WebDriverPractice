3df0b4ca5105d9c0af060c33ab254788adc0050d
Slawomir Was <was.slawek@gmail.com>
2025-02-10 23:08:56 +0100

    add models

---
+++ WebDriverPractice/Business/Models/CompanyModel.cs
@@ -0,0 +1,14 @@
+﻿namespace WebDriverPractice.Business.Models
+{
+	public class CompanyModel
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public string Username { get; set; }
+		public string Email { get; set; }
+		public AddressModel Address { get; set; }
+		public string Phone { get; set; }
+		public string Website { get; set; }
+		public CompanyModel Company { get; set; }
+	}
+}
---
+++ WebDriverPractice/Business/Models/UserModel.cs
@@ -0,0 +1,8 @@
+﻿namespace WebDriverPractice.Business.Models
+{
+	public class UserModel
+	{
+		public string Name { get; set; }
+		public string CatchPhrase { get; set; }
+	}
+}
