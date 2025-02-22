3107224e0badde29168e160f8c657070bde339ca
Slawomir Was <was.slawek@gmail.com>
2025-02-22 15:11:23 +0100

    clean configmanager

--- WebDriverPractice/Core/Config/ConfigManager.cs
+++ WebDriverPractice/Core/Config/ConfigManager.cs
@@ -6,13 +6,8 @@ namespace WebDriverPractice.Core.Config
 {
 	public static class ConfigManager
 	{
-		//private static readonly IConfigurationRoot _configuration = new ConfigurationBuilder()
-		//   .SetBasePath(Directory.GetCurrentDirectory())
-		//   .AddJsonFile(GetConfigFilePath(), optional: false, reloadOnChange: true)
-		//   .Build();
-
 		private static readonly IConfigurationRoot _configuration = new ConfigurationBuilder()
-		   .SetBasePath(GetConfigDirectory()) // Dynamically locate the bin folder
+		   .SetBasePath(GetConfigDirectory())
 		   .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
 		   .Build();
 
@@ -26,22 +21,11 @@ namespace WebDriverPractice.Core.Config
 		{
 			return bool.TryParse(_configuration[key], out var result) && result;
 		}
-
-		//private static string GetConfigFilePath()
-		//{
-		//	Log.Information("Getting File Path.");
-		//	string basePath = Directory.GetParent(AppDomain.CurrentDomain.BaseDirectory)!.Parent!.Parent!.Parent!.FullName;
-		//	string configFileName = "appsettings.json";
-		//	string configFilePath = Path.Combine(basePath, configFileName);
-
-		//	return configFilePath;
-		//}
 		
 		private static string GetConfigDirectory()
 		{
 			Log.Information("Getting configuration file path.");
 
-			// Get the executing assembly's directory (inside bin/debug or bin/release)
 			string? assemblyLocation = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
 
 			if (string.IsNullOrEmpty(assemblyLocation))
