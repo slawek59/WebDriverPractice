9203e81b92aa3f9e31745f28512472ce5e846641
Slawomir Was <was.slawek@gmail.com>
2025-02-27 02:20:27 +0100

    pipeline 13

--- WebDriverPractice/Core/Helpers/ScreenshotMaker.cs
+++ WebDriverPractice/Core/Helpers/ScreenshotMaker.cs
@@ -7,7 +7,7 @@ namespace WebDriverPractice.Core.Helpers
 		public static string TakeBrowserScreenshot(ITakesScreenshot driver)
 		{
 			var now = DateTime.Now.ToString("yyyy-MM-dd_hh-mm-ss-fff");
-			var screenshotPath = Path.Combine(Environment.CurrentDirectory, $"Display_{now}.png");
+			var screenshotPath = Path.Combine("TestResults", $"Display_{now}.png");
 			driver.GetScreenshot().SaveAsFile(screenshotPath);
 
 			return screenshotPath;
